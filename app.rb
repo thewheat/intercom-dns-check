3e613059c2c5516c422dc81c2de58bf19f6e5d33
Timothy Lim <tim.lim@intercom.io>
2018-12-21 13:32:50 +0800

    Better error messages

--- app.rb
+++ app.rb
@@ -37,28 +37,6 @@ class DnsCheck
   end
 end
 
-def test
-	url = 'intercom._domainkey.thewheatfield.org'
-	url = 'intercom._domainkey.thewheatfield.org.thewheatfield.org'
-	begin
-		      resolver = Dnsruby::Resolver.new(packet_timeout: 5, retry_delay: 5, retry_times: 3,
-                                       nameserver: NAMESERVERS)
-
-	  r = Resolv::DNS.open do |dns|
-	    dns.getresource(url, Resolv::DNS::Resource::IN::TXT)
-#	    dns.getresource(url, Resolv::DNS::Resource::IN::CNAME)
-	  end
-	  puts r.inspect
-	  #puts r.name.to_s # => return alias domain
-	rescue Resolv::ResolvError => e
-	  # handle error
-	end
-end
-
-def getHost(url)
-	myUri = URI.parse("http://#{url}")
-	myUri.host
-end
 
 def get_intercom_dkim_base
 	return "intercom._domainkey"	
@@ -181,13 +159,15 @@ def check_dkim(domain)
 			dkim_output += "<div>Nameservers: [#{nameservers.join('][')}]</div>"
 		end
 		if cname 
-			dkim_output += "<div class='success'>CNAME exists for #{get_intercom_dkim(root_domain)}. <BR><code class='indent'>#{cname}</code> <br> Ensure it is the same as what you see in Intercom</div>"
+			dkim_output += "<div class='success'>CNAME exists for <code>#{get_intercom_dkim(root_domain)}</code>. <BR><code class='indent'>#{cname}</code> <br> Ensure it is the same as what you see in Intercom</div>"
+		else
+			dkim_output += "<div class='error'>No CNAME exists for <code>#{get_intercom_dkim(root_domain)}</code>. <br> #{dns_check_message}</div>"
 		end
 		if dns_hosts && has_cloudflare_as_dns
 			dkim_output += "<div class='warn'>It looks like you're using Cloudflare. If you're having issues, ensure that you have <a href='https://support.cloudflare.com/hc/en-us/articles/200169056-CNAME-Flattening-RFC-compliant-support-for-CNAME-at-the-root'>disabled CNAME flattening</a> and that the DNS entry has a <a href='https://support.cloudflare.com/hc/en-us/articles/200169626-What-subdomains-are-appropriate-for-orange-gray-clouds-'>gray cloud to ensure the traffic goes to Intercom</a></div>"
 		end
 		if double_domain
-			dkim_output += "<div class='error'>CNAME exist for #{get_intercom_dkim_double_domain(root_domain)}. <BR><code class='indent'>#{double_domain}</code>.</div>"
+			dkim_output += "<div class='error'>CNAME exist for <code>#{get_intercom_dkim_double_domain(root_domain)}</code>. <BR><code class='indent'>#{double_domain}</code>.</div>"
 			dkim_output += "<div class='error'>This is likely an incorrect entry. If you have specified <code>#{get_intercom_dkim(root_domain)}</code> in your DNS server try use <code>#{get_intercom_dkim_base}</code> instead. Some DNS servers automatically add the domain name which can cause this error</div>"
 		end
 
@@ -207,11 +187,15 @@ def check_dkim(domain)
 	return dkim_output
 end
 
+def dns_check_message
+	"Ensure you are modifying the correct DNS server. Check the DNS provider or nameserver details to ensure it matches the server you're modifying"
+end
 def abort_message
 	"Can't complete check. Ensure you have specified a correct domain / URL"
 end
 
-def check_custom_domain (custom_domain)
+def check_custom_domain (custom_domain, use_ssl)
+	custom_domain_ssl = (use_ssl && use_ssl.to_s.strip.downcase == "yes")
 	custom_domain_output = ""
 	if !custom_domain.nil? && !custom_domain.strip.empty?
 		custom_domain_output += "<div>Checking Articles Custom Domain: <code>#{custom_domain}</code>....</div>"
@@ -239,31 +223,11 @@ def check_custom_domain (custom_domain)
 			custom_domain_output += "<div class='error'>Problem retrieving nameservers. Will continue processing but may not be 100% accurate</div>"
 		end
 
-
-		begin
-			cname = get_cname(custom_domain)
-		rescue
-			custom_domain_output += "<div class='error'>Problem retrieving CNAME on domain. #{abort_message}</div>"
-			return custom_domain_output
-		end
-
-		if cname.nil? || cname.empty?
-			className = "error"
-			custom_domain_output += "<div class='error'>No CNAME value set up. It needs to be <br><code class='indent'>#{get_articles_cname_value}</code></div>"
-		elsif cname != get_articles_cname_value
-			className = "error"
-			custom_domain_output += "<div class='error'>Looks like you have an incorrect CNAME value. It needs to be <br><code class='indent'>#{get_articles_cname_value}</code></div>"
-		else
-			className = "success"
-			custom_domain_output += "<div class='success'>CNAME looks correctly configured</div>"
-		end
-		custom_domain_output += "<div class='#{className}'>CNAME exists with value <BR><code class='indent'>#{cname}</code></div>" unless (cname.nil? || cname.empty?)
-
-		custom_domain_output += "<HR>"
-
 		if dns_hosts
+			has_cloudflare_as_dns = has_cloudflare_as_dns(dns_hosts)
 			dns_hosts = dns_hosts.first if dns_hosts.count == 1
 		end
+
 		if root_domain && root_domain != custom_domain
 			custom_domain_output += "<div>Root domain: <code>#{root_domain}</code></div>"
 		end
@@ -273,6 +237,37 @@ def check_custom_domain (custom_domain)
 		if nameservers
 			custom_domain_output += "<div>Nameservers: [#{nameservers.join('][')}]</div>"
 		end
+
+		begin
+			cname = get_cname(custom_domain)
+		rescue
+			unless custom_domain_ssl
+				custom_domain_output += "<div class='error'>Problem retrieving CNAME on domain. #{abort_message}</div>"
+				return custom_domain_output 
+			end
+		end
+
+		if custom_domain_ssl
+			if cname && has_cloudflare_as_dns
+				custom_domain_output += "<div class='info'>It looks like you're using Cloudflare but we can detect a CNAME. If you're having issues, ensure that the DNS entry has <a href='https://support.cloudflare.com/hc/en-us/articles/200169626-What-subdomains-are-appropriate-for-orange-gray-clouds-'>an  orange cloud to ensure the traffic goes to Cloudflare to provide the HTTPS</a></div>"
+			end
+
+			custom_domain_output += "<div class='info'>For Custom Domains with HTTPS/SSL this will be dependant on your configuration. <ul><li>Ensure you have followed the steps <a href='https://developers.intercom.com/installing-intercom/docs/set-up-your-custom-domain'>in the docs</a></li><li>Or if you have a custom setup get in touch with Intercom, specifying details of your setup</li><li>Check the DNS providers listed to ensure that you are configuring the correct DNS as well</li></ul></div>"
+
+		else
+			if cname.nil? || cname.empty?
+				className = "error"
+				custom_domain_output += "<div class='error'>No CNAME value set up. It needs to be <br><code class='indent'>#{get_articles_cname_value}</code> <br>#{dns_check_message}</div>"
+			elsif cname != get_articles_cname_value
+				className = "error"
+				custom_domain_output += "<div class='error'>Looks like you have an incorrect CNAME value. It needs to be <br><code class='indent'>#{get_articles_cname_value}</code> <br>#{dns_check_message}</div>"
+			else
+				className = "success"
+				custom_domain_output += "<div class='success'>CNAME looks correctly configured</div>"
+			end
+			custom_domain_output += "<div class='#{className}'>CNAME exists with value <BR><code class='indent'>#{cname}</code></div>" unless (cname.nil? || cname.empty?)
+		end
+
 		custom_domain_output += "<div class='footnote'>You can also <a href='https://mxtoolbox.com/SuperTool.aspx?action=cname%3a#{custom_domain}'>check your your domain via MXToolbox</a></div>"
 	end
 	return custom_domain_output
@@ -290,21 +285,27 @@ get '/' do
 	output += "div.section h3 { margin: 0;  }"
 	output += "div.section code.indent { padding-left: 1em;  }"
 	output += "div.section .footnote { margin-top: 1em; font-size: 0.8em; }"
-	output += "div.section .log { background-color: #ccc; }"
+	output += "div.section .log { background-color: #ddd; }"
+	output += "form { margin-top: 1em; }"
+	output += "form label { font-weight: bold; }"
+	output += "form .form-item { margin-bottom: 0.4em; }"
+	output += "form .form-item input[type=text] { display: block; width: 20em; font-size: 2em; }\n"
+	output += "input[type=submit] { font-size: 16px; border-radius: 5px; background-color: #2D6DC5; color: #ffffff; font-weight: bold; padding: 5px 18px 7px; border: 1px solid rgba(29,54,75,0.2);}"
+	output += "input[type=submit]:hover { background-color: #1946C4; }"
+
 
 	output += "</style></head><body>"
 
 	dkim_output = check_dkim(params["dkim_domain"])
 
-	custom_domain_output = check_custom_domain(params["custom_domain"])
-
+	custom_domain_output = check_custom_domain(params["custom_domain"], params["custom_domain_ssl"])
 	
 	output += "<div class='section'>"
 	output += "<h3>DKIM Domain verification for sending Messages</h3>"
 	output += "<a href='https://www.intercom.com/help/configure-intercom-for-your-product-or-site/configure-intercom-for-your-team/a-guide-to-sending-email-from-your-own-address'>Intercom Docs</a>"
 	output += "<form method='get' action='/'>"
-	output += "<label>Domain to check: <input type='text' name='dkim_domain'></label>"
-	output += "<input type='submit' value='Check domain!'>"
+	output += "<div class='form-item'><label>Domain to check: <input type='text' name='dkim_domain' value='#{params["dkim_domain"]}'></label></div>"
+	output += "<input type='submit' value='Check domain!'> (Enter in the format of <code>subdomain.domain.com</code>: remove any http / https prefix)"
 	output += "</form>"
 	output += output_log(dkim_output)
 	output += "</div>"
@@ -312,8 +313,12 @@ get '/' do
 	output += "<h3>Custom Domain for Articles</h3>"
 	output += "<a href='https://developers.intercom.com/installing-intercom/docs/set-up-your-custom-domain'>Intercom Docs</a>"
 	output += "<form method='get' action='/'>"
-	output += "<label>Domain to check: <input type='text' name='custom_domain'></label>"
+	output += "<div class='form-item'>Setting up HTTPS/SSL?"
+	output += "<label><input type='radio' name='custom_domain_ssl' value='no' #{(params["custom_domain_ssl"] == "yes" ? "" : "checked")}>No</label>"
+	output += "<label><input type='radio' name='custom_domain_ssl' value='yes' #{(params["custom_domain_ssl"] == "yes" ? "checked" : "")}>Yes</label></div>"
+	output += "<div class='form-item'><label>Domain to check: <input type='text' name='custom_domain' value='#{params["custom_domain"]}'></label></div>"
 	output += "<input type='submit' value='Check Domain!'>"
+	output += " (Enter in the format of <code>subdomain.domain.com</code>: remove any http / https prefix)"
 	output += "</form>"
 	output += output_log(custom_domain_output)
 	output += "</div>"
